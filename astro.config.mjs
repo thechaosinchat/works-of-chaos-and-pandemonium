7dc7731c3c53c2399f77e9fe4c3403446cbefe5c
TheChaosInChat <goldenaqian@gmail.com>
2026-09-17 12:12:56 -0400

    Add Astro configuration

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://thechaosinchat.github.io',
+  base: '/works-of-chaos-and-pandemonium',
+  output: 'static'
+});
